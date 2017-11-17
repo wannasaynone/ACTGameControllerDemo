131a4af899f2c9d8a8783cebaa7935b0d5d3620f
KAHA <wannasaynone@gmail.com>
2017-11-17 15:20:41 +0800

    Project Set

---
+++ Assets/HitBox.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitBox : MonoBehaviour {
+
+    [SerializeField]
+    private Controller Attacker = null;
+    [SerializeField]
+    private bool isHeavyHit = false;
+
+    private void Awake()
+    {
+        if (Attacker == null)
+            Debug.LogError("Attack == null");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameManager.insance.Combat_Hit(Attacker.gameObject, collision.gameObject, isHeavyHit);
+    }
+}
